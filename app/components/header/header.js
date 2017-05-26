4e7baf45de7d92a22b13db2e4274564df5908ac0
vpurush <vijayaraghavan1987@gmail.com>
2017-05-26 06:39:44 +0530

    made login page responsive

--- app/components/header/header.js
+++ app/components/header/header.js
@@ -8,12 +8,18 @@ var Header = React.createClass({
         var user = ([]);
         var timelineSearch = ([]);
 
+        var logo = [];
+        if(location.hash.indexOf("/login") == -1){
+            console.log("inside logo");
+            logo = (<Link to={'/timelinelist'}><span className="logo">Timeline</span></Link>);
+        }
+
         if(this.props.username){
             user = (<span className="username">Welcome, {this.props.username}!</span>);
             timelineSearch = (<input className="timeline-filter form-control" type="text" onChange={this.props.timelineFilterChange} placeholder="Type to filter in this timeline" />);
         }
         var html = (<header>
-                        <Link to={'/timelinelist'}><span className="logo">Timeline</span></Link>
+                        {logo}
                         {timelineSearch}
                         {user}
                     </header>);
--- app/components/login/login.js
+++ app/components/login/login.js
@@ -43,35 +43,39 @@ module.exports = React.createClass({
         html = (
             <div className="login">
                 <div className="logo">Timeline</div>
-                <div>
-                    <Tabs defaultActiveKey={1} id="login_signup" onClick={this.clearError}>
-                        <Tab eventKey={1} title="Login">
-                            <form>
-                                <fieldset>
-                                    {/*<legend>Please login</legend>*/}
-                                    <div className="error-message">{this.state.error}&nbsp;</div>
-                                    <div className="form-group">
-                                        <label htmlFor="title">Username</label>
-                                        <input type="text" className="form-control" id="title" placeholder="Username" value={this.state.username} onChange={this.setUsername}/>
-                                    </div>
-                                    <button type="button" className="btn btn-default" onClick={this.submit}>Login</button>
-                                </fieldset>
-                            </form>
-                        </Tab>
-                        <Tab eventKey={2} title="Signup">
-                            <form>
-                                <fieldset>
-                                    {/*<legend>Please login</legend>*/}
-                                    <div className="error-message">{this.state.error}&nbsp;</div>
-                                    <div className="form-group">
-                                        <label htmlFor="title">Username</label>
-                                        <input type="text" className="form-control" id="title" placeholder="Username" value={this.state.username} onChange={this.setUsername}/>
-                                    </div>
-                                    <button type="button" className="btn btn-default" onClick={this.signup}>Signup</button>
-                                </fieldset>
-                            </form>
-                        </Tab>
-                    </Tabs>
+                <div className="row">
+                    <div className="col-md-3"></div>
+                    <div className="col-md-6 col-xs-12">
+                        <Tabs defaultActiveKey={1} id="login_signup" onClick={this.clearError}>
+                            <Tab eventKey={1} title="Login">
+                                <form>
+                                    <fieldset>
+                                        {/*<legend>Please login</legend>*/}
+                                        <div className="error-message">{this.state.error}&nbsp;</div>
+                                        <div className="form-group">
+                                            <label htmlFor="title">Username</label>
+                                            <input type="text" className="form-control" id="title" placeholder="Username" value={this.state.username} onChange={this.setUsername}/>
+                                        </div>
+                                        <button type="submit" className="btn btn-default" onClick={this.submit}>Login</button>
+                                    </fieldset>
+                                </form>
+                            </Tab>
+                            <Tab eventKey={2} title="Signup">
+                                <form>
+                                    <fieldset>
+                                        {/*<legend>Please login</legend>*/}
+                                        <div className="error-message">{this.state.error}&nbsp;</div>
+                                        <div className="form-group">
+                                            <label htmlFor="title">Username</label>
+                                            <input type="text" className="form-control" id="title" placeholder="Username" value={this.state.username} onChange={this.setUsername}/>
+                                        </div>
+                                        <button type="submit" className="btn btn-default" onClick={this.signup}>Signup</button>
+                                    </fieldset>
+                                </form>
+                            </Tab>
+                        </Tabs>
+                    </div>
+                    <div className="col-md-3"></div>
                 </div>
             </div>
         );
